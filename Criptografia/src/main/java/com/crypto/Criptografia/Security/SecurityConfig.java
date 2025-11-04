b2175c36c2df44227acca21576f19f298c0a7862
Samuel Ângelo <samuel.aasilva07@gmail.com>
2025-11-04 18:49:38 -0300

    Fix formatting and comments in SecurityConfig

--- Criptografia/src/main/java/com/crypto/Criptografia/Security/SecurityConfig.java
+++ Criptografia/src/main/java/com/crypto/Criptografia/Security/SecurityConfig.java
@@ -10,9 +10,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // desativa proteção CSRF
+                .csrf(csrf -> csrf.disable()
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // permite acesso a todas as rotas
+                        .anyRequest().permitAll()
                 );
         return http.build();
     }
